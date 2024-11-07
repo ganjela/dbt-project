60f3046a0eaa88313679bca2d783799138a50fa7
ganjela <nikaganjelashvili@gmail.com>
2024-11-07 21:48:13 +0400

    add raw_resellers_type1.sql

--- analytics/models/raw/raw_resellers_type1.sql
+++ analytics/models/raw/raw_resellers_type1.sql
@@ -1,3 +1,19 @@
+{{ config(materialized='table') }}
+
+SELECT 
+    "Product name" AS product_name,
+    "Quantity" AS quantity,
+    "Total amount" AS total_amount,
+    "Sales Channel" AS sales_channel,
+    "Customer First Name" AS customer_first_name,
+    "Customer Last Name" AS customer_last_name,
+    "Customer Email" AS customer_email,
+    "Series City" AS series_city,
+    "Created Date" AS created_date,
+    "Reseller ID" AS reseller_id,
+    "Transaction ID" AS transaction_id
+FROM 
+    {{ source('parquet_source', 'resellers_type1') }}
 
 
 
